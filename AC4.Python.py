bc460c629a94dbef22b7e5ed2c63556e5e75336c
hbohera <hbohera@institutmvm.cat>
2021-01-21 14:43:43 +0100

    initial commit

--- AC4.Python.py
+++ AC4.Python.py
@@ -7,16 +7,34 @@ def main():
         aux = x
         for j in range (LINES-aux):
             print(" ", end=" ")
-        for k in range(x-1):
+        for k in range(x):
             print(aux, end=" ")
             aux-=1
         if x != 1:
-            for h in range(x):
+            for h in range(x-1):
                 print(count, end=" ")
                 count+=1
         x+=1
-        count=1
+        count=2
         aux-=1
         print()
+    x = 6
+    aux = 0
+    count = 2
+    for i in range(LINES-1):
+        aux = x
+        for j in range (LINES-aux):
+            print(" ", end=" ")
+        for k in range(x):
+            print(aux, end=" ")
+            aux-=1
+        for h in range(x-1):
+            print(count, end=" ")
+            count+=1
+        print()
+        x-=1
+        count=2
+        aux-=1
+
 if __name__ == "__main__":
     main()
