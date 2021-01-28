3b6e5bbd9ae8b0ded9018f5f6736c8c182216558
hbohera <hbohera@institutmvm.cat>
2021-01-28 12:47:13 +0100

    commit

--- AC12.Python.py
+++ AC12.Python.py
@@ -1,17 +1,29 @@
 NUMBERS = 10
 def main():
-    number_list = []
     count_e = 0
     count_o = 0
-
+    number_list = []
+    even = []
+    odd = []
     for i in range(NUMBERS):
-        number_list[i] = int(input("Introduce el número"))
-        while number_list[i] < 0 or number_list[i] > 10:
-            number_list[i] = int(input("Introduce el número entre 0 y 10 incluidos: "))
+        number = int(input("Introduce el número: "))
+        while number < 0 or number > 10:
+            number = int(input("Introduce el número entre 0 y 10 incluidos: "))
+        number_list.append(number)
     for i in range(NUMBERS):
-        if number_list[i] MOD 2 == 0:
-            even[count_e] = numbers_list[i]
+        if number_list[i] % 2 == 0:
+            even.append(number_list[i])
             count_e += 1
-        else
+        else:
+            odd.append(number_list[i])
+            count_o += 1
+    print("Los números pares son:", end=" ")
+    for i in range(count_e):
+        print(even[i], end=" ")
+    print()
+    print("Los números impares son:", end=" ")
+    for j in range(count_o):
+        print(odd[j], end=" ")
+
 if __name__ == "__main__":
     main()
