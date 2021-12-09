58862df11b420f54969d917496b9a07e9ddfd9e8
Hbohera <77778415+Hbohera@users.noreply.github.com>
2021-12-09 11:04:03 +0100

    Update and rename AC9.Python.py to Ejercicio2

--- AC9.Python.py
+++
@@ -1,7 +0,0 @@
-def main():
-   mbytes = int(input("Introduce los MegaBytes: ")) # Variable que almacená los MB introducidos por el usuario
-   bytes = mbytes * 1000000 # Se hace la conversión a bytes y se guarda en la variable bytes
-   print(mbytes, "MB son", bytes, "bytes") # Se muestra al usuario el resultado
-
-if __name__ == "__main__":
-    main()
