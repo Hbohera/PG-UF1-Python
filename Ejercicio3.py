f21cd26e97a0b03261a08b1074ef77572d275ab8
Hbohera <77778415+Hbohera@users.noreply.github.com>
2021-12-09 11:49:52 +0100

    Update Ejercicio3.py

--- Ejercicio3.py
+++ Ejercicio3.py
@@ -1,1 +1,21 @@
+def main():
+    repeat= int(input("¿Cuantos usuarios quieres introducir?: "))
+    username = list()
+    departament = list()
+    classroom = list()
+    for i in range(repeat):
+        username.append(str(input("\n¿Cual es el nombre del alumno?: ")))
+        departament.append(str(input("¿Cual es el nombre del departamento?: ")))
+        classroom.append(int(input("¿Que número tiene la clase?: ")))
+        while 1 < classroom[-1] > 15:
+            classroom.append(int(input("¿Vuelve a introducir el número que tiene la clase?(1-15): ")))
+    students = {
+        "username": username,
+        "departament": departament,
+        "classroom": classroom
+    }
+    for y in students:
+            print("\n |", students[y], end=" | \n")
 
+if __name__ == "__main__":
+    main()
