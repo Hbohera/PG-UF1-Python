d489c9ec386215263b243a8e6d36a2946873c135
Hbohera <77778415+Hbohera@users.noreply.github.com>
2021-12-09 11:04:15 +0100

    Update Ejercicio1.py

--- Ejercicio1.py
+++ Ejercicio1.py
@@ -1,10 +1,19 @@
 def main():
-    sentence = input("Introduce la frase que quieres ver al reves: ")
-    print("")
-    firstchar= input("Cambia la primera letra de lo que has introducido: ")
-    sentence_new= firstchar + sentence[1:]
-    print("")
-    print("Tu frase al reves es=", sentence_new[::-1])
+    n1 = int(input("Introduce el primer número: "))
+    while n1 < 0:
+        n1 = int(input("Vuelve a introducir el número: "))
+    n2 = int(input("Introduce el segundo número: "))
+    while n2 < 0:
+        n2 = int(input("Vuelve a introducir el número: "))
+    numbers = list()
+    numbers.append(n1)
+    numbers.append(n2)
+    if numbers[0] < numbers[1]:
+        print("El primero número es mas pequeño que el segundo.")
+    else:
+        print("Error: El segundo número es mas pequeño que el primero")
+    numbers.sort()
+    print(numbers)
 
 if __name__ == "__main__":
     main()
